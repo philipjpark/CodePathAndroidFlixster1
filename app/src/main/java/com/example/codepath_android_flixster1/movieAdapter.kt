2113bfa6b4b16cea5a0bf3a83e3159e2fdb71d20
philipjpark <philipparknyc@gmail.com>
2022-03-04 21:15:34 -0500

    Completed unit 2

--- app/src/main/java/com/example/codepath_android_flixster1/movieAdapter.kt
+++ app/src/main/java/com/example/codepath_android_flixster1/movieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.codepath_android_flixster1
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -28,15 +30,27 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
             private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
             private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
             private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+            init {
+                itemView.setOnClickListener(this)
+            }
 
             fun bind(movie: Movie) {
                 tvTitle.text = movie.title
                 tvOverview.text = movie.overview
                 Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
             }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            //Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
+    }
     }
